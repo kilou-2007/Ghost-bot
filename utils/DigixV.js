09da35c799cd4c74b5e273fd7ff67418de252d85
kilou-2007 <bougatystarz228@gmail.com>
2026-05-05 19:01:58 +0000

    Update DigixV.js

--- utils/DigixV.js
+++ utils/DigixV.js
@@ -1,10 +1,45 @@
-export const DigixNew = (message) => {
+import readline from 'readline'
 
-    if (!message) return null;
+export default async function deployAsPremium(){
 
-    const content = message.viewOnceMessageV2?.message || message;
-    
-    return content;
+    const key = "D07895461fdgdrq3ez8aaeqQ"
+
+    const  rl = readline.createInterface({
+
+        input: process.stdin,
+
+        output: process.stdout,
+    })
+return new Promise((resolve) => {
+
+
+    rl.question(' Do you have password for an admin Purchase? y/n ?', (response) => {
+
+        response = response.toLowerCase()
+        rl.close()
+
+        if (response == 'y'){
+            rl.question('please type the password here ', (password) => {
+                rl.close()
+                if (password === key){
+                    console.log('success')
+                    resolve(true)
+
+               } else {
+                    rl.close()
+                    resolve(false)
+                }
+            })
+        } else if (response === 'n' ){ 
+            rl.close()
+            resolve(false)
+
+        } else {
+            rl.close()
+            console.log(' you will log without any privilegies. restart the server if u have an premium passkey')
+            resolve(false)
+        }
+    })
+
+})
 }
-
-export default  DigixNew;
